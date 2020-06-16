9e19065f43e65e74fb699e94e7d397abf3f75aef
Ray Eichler <ray.eichler@yahoo.com>
2020-06-16 17:31:38 -0500

    Collatz Sequence for assignment on 6/16/2020

---
+++ Collatz/CollatzSequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CollatzSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string selection;
+
+            Console.WriteLine("Please enter number to perform Collatz Sequnce");
+
+            selection = Console.ReadLine();
+
+            number = int.Parse(selection);
+
+            for(;number > 1;)
+            {
+                Console.WriteLine(number);
+
+                if(number % 2 == 0)
+                    number = number / 2;
+                else
+                    number = (number * 3) + 1;
+            }
+        }
+    }
+}
