409f94b890f42d233e95acfd06e99a554c377826
Ray Eichler <rayeichler@Rays-MacBook-Pro.local>
2020-06-17 02:19:42 -0500

    --help

---
+++ staircase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("  #");
+            // Console.WriteLine(" ##");
+            // Console.WriteLine("###");
+
+            for(int row = 0; row < 8; row++)
+            {
+                for(int col = 0; col < 8; col++)
+                {
+                    if(col + row < 7)
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write('#');
+                    }
+                }
+                Console.Write('\n');
+            }
+        }
+    }
+}
