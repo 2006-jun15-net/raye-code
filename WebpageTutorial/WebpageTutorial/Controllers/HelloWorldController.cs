62550e98e7e999177872e137e24a0a458f0ebb19
Ray Eichler <ray.eichler@yahoo.com>
2020-07-02 15:44:20 -0500

    Haven't pushed in awhile

---
+++ WebpageTutorial/WebpageTutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebpageTutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default Action";
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
---
+++ WebpageTutorial/WebpageTutorial/Data/MvcMovieContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace WebpageTutorial.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options): base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
